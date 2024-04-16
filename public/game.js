533e8eba45345969058449190816eeb0b7294388
Jovan <jovanstosic012@gmail.com>
2024-04-15 20:48:07 -0500

    Add the basic code so when a laser is near an enemy the enemy is killed

--- public/game.js
+++ public/game.js
@@ -35,6 +35,17 @@ function deleteLaster(child) {
   app.stage.removeChild(child);
 }
 
+function isColliding(a, b) {
+  let ab = a.getBounds();
+  let bb = b.getBounds();
+  return (
+    ab.x + ab.width > bb.x &&
+    ab.x < bb.x + bb.width &&
+    ab.y + ab.height > bb.y &&
+    ab.y < bb.y + bb.height
+  );
+}
+
 function fireLaser() {
   let laserBlast = PIXI.Sprite.from("shipLaser.png");
 
@@ -47,6 +58,10 @@ function fireLaser() {
   app.stage.addChild(laserBlast);
 
   app.ticker.add(() => {
+    if (isColliding(laserBlast, testEnemy)) {
+      app.stage.removeChild(testEnemy);
+    }
+
     laserBlast.y -= 10;
   });
 
