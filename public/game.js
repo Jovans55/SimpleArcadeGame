677cbf813d92b6cacadaa91cc05e884787c1b5eb
Jovan <jovanstosic012@gmail.com>
2024-04-15 19:46:18 -0500

    Finished giving the website some simple styling to avoid starting at an eye sore

--- public/game.js
+++ public/game.js
@@ -6,3 +6,10 @@ await app.init({ width: 700, height: 700 });
 document.body.appendChild(app.canvas);
 
 await PIXI.Assets.load("ship.png", "tempEnemy.png");
+
+let player = PIXI.Sprite.from("ship.png");
+
+player.x = 100;
+player.y = 100;
+
+app.stage.addChild(player);
