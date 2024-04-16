5af3b2b628a89274ab3148203cc9ec4933271b06
Jovan <jovanstosic012@gmail.com>
2024-04-15 19:44:54 -0500

    Finished giving the website some simple styling to avoid starting at an eye sore

--- public/game.js
+++ public/game.js
@@ -1,1 +1,8 @@
-console.log("AM I WORKING???");
+console.log("Game spinning up");
+
+const app = new PIXI.Application();
+
+await app.init({ width: 700, height: 700 });
+document.body.appendChild(app.canvas);
+
+await PIXI.Assets.load("ship.png", "tempEnemy.png");
